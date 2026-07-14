4f4fbf99405657f8a521787ea3810aa8cfd72a0a
peshub062025-cmyk <peshub062025@gmail.com>
2026-07-14 20:47:19 +0700

    Refactor Players cog and command setup
    Refactor Players cog to include initialization and command structure.

--- cogs/players.py
+++ cogs/players.py
@@ -1,32 +1,37 @@
 import discord
-
 from discord import app_commands
 from discord.ext import commands
 
-from sqlalchemy.orm import Session
-
-from database.db import SessionLocal
-from database.models import Player
-
-from utils.checks import check_admin
-from utils.checks import check_channel
-
+from utils.checks import check_admin, check_channel
 from ui.player_modal import PlayerModal
-@app_commands.command(
-    name="themnguoi",
-    description="Thêm người chơi"
-)
-async def themnguoi(
-    self,
-    interaction: discord.Interaction
-):
 
-    if not await check_channel(interaction):
-        return
 
-    if not await check_admin(interaction):
-        return
+class Players(commands.Cog):
 
-    await interaction.response.send_modal(
-        PlayerModal()
+    def __init__(self, bot):
+        self.bot = bot
+
+    @app_commands.command(
+        name="themnguoi",
+        description="Thêm người chơi"
+    )
+    async def themnguoi(
+        self,
+        interaction: discord.Interaction
+    ):
+
+        if not await check_channel(interaction):
+            return
+
+        if not await check_admin(interaction):
+            return
+
+        await interaction.response.send_modal(
+            PlayerModal()
+        )
+
+
+async def setup(bot):
+    await bot.add_cog(
+        Players(bot)
     )
