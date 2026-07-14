65c3b3933e2f4eaba5a2819400bfbae6b9833766
peshub062025-cmyk <peshub062025@gmail.com>
2026-07-14 10:31:14 +0700

    Add channel and admin checks for interactions

--- utils/checks.py
+++ utils/checks.py
@@ -1,3 +1,32 @@
-is_admin()
+import discord
 
-check_channel()
+from config import BOT_CHANNEL_ID
+from config import ADMIN_USER_IDS
+
+
+async def check_channel(interaction: discord.Interaction):
+
+    if interaction.channel.id != BOT_CHANNEL_ID:
+
+        await interaction.response.send_message(
+            "⛔ Bot chỉ hoạt động trong kênh đã chỉ định.",
+            ephemeral=True
+        )
+
+        return False
+
+    return True
+
+
+async def check_admin(interaction: discord.Interaction):
+
+    if interaction.user.id not in ADMIN_USER_IDS:
+
+        await interaction.response.send_message(
+            "⛔ Bạn không có quyền sử dụng lệnh này.",
+            ephemeral=True
+        )
+
+        return False
+
+    return True
