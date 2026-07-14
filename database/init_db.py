958cc144662493005f9aa04a3abf5d1c6d3f8de4
peshub062025-cmyk <peshub062025@gmail.com>
2026-07-14 22:32:12 +0700

    Update init_db.py

--- database/init_db.py
+++ database/init_db.py
@@ -1,5 +1,9 @@
-from database.db import engine
-from database.models import Base
+from database.db import Base, engine
+
+# Import tất cả models để SQLAlchemy biết cần tạo bảng nào
+from database.models import Player, Season, Match
+
 
 def create_database():
     Base.metadata.create_all(bind=engine)
+    print("✅ SQLite database đã sẵn sàng.")
