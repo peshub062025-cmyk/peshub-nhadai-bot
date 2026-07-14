494b83a5bde4421181611be93070f36aaf1db27c
peshub062025-cmyk <peshub062025@gmail.com>
2026-07-14 22:22:57 +0700

    Update db.py

--- database/db.py
+++ database/db.py
@@ -1,23 +1,14 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-from config import DATABASE_URL
+engine = create_engine(
+    "sqlite:///database/peshub.db",
+    echo=False,
+    future=True
+)
 
-Base = declarative_base()
-
-engine = None
-SessionLocal = None
-
-if DATABASE_URL:
-
-    engine = create_engine(
-        DATABASE_URL,
-        pool_pre_ping=True
-    )
-
-    SessionLocal = sessionmaker(
-        autocommit=False,
-        autoflush=False,
-        bind=engine
-    )
+SessionLocal = sessionmaker(
+    bind=engine,
+    autoflush=False,
+    autocommit=False
+)
