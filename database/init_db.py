71be0b4c16afed5d9b4f44face3d37714e8e655c
peshub062025-cmyk <peshub062025@gmail.com>
2026-07-14 22:23:40 +0700

    Refactor database initialization to import Base from models

--- database/init_db.py
+++ database/init_db.py
@@ -1,15 +1,5 @@
 from database.db import engine
-from database.db import Base
-
-import database.models
-
+from database.models import Base
 
 def create_database():
-
-    if engine is None:
-        print("⚠ Database chưa được cấu hình.")
-        return
-
     Base.metadata.create_all(bind=engine)
-
-    print("✅ Database sẵn sàng.")
